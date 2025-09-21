658a70a4996c2919d10cc8928011388f16cb294e
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-09-21 19:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+    const btn = document.getElementById("openModal");
+    const closeBtn = document.querySelector(".close-modal");
+
+    // Open modal
+    btn.onclick = () => {
+      modal.style.display = "block";
+    };
+
+    // Close modal with X
+    closeBtn.onclick = () => {
+      modal.style.display = "none";
+    };
+
+    // Close modal on outside click
+    window.onclick = (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
